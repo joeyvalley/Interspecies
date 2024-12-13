77b7eab316690692f37e9f8c048b9316d843e2d4
Joseph Valle <josephrvalle@gmail.com>
2024-12-13 16:47:37 -0500

    header done, working on home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import Header from './components/Header';
+
 import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home';
+
+import Header from './components/Header';
+import VideoBackground from './components/VideoBackground';
 
 import './styles/global.css';
 
@@ -7,9 +12,10 @@ function App() {
   return (
     <div className="container">
       <Header />
-      {/* <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-      </Routes> */}
+      <VideoBackground />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,8 @@
+export default function VideoBackground() {
+  return (
+    <div className="video-container">
+      <video autoplay="True" muted loop src="/assets/homepage-loop.mp4" type="video/mp4">
+      </video>
+    </div>
+  );
+}
