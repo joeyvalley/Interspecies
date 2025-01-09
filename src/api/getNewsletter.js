4e405136e661c64da7b367ea8163a24a37950dfe
Joseph Valle <gatekeeper@Josephs-MacBook-Pro.local>
2025-01-08 22:58:12 -0500

    testing environment variables

--- src/api/getNewsletter.js
+++ src/api/getNewsletter.js
@@ -1,7 +1,7 @@
 import api from './apiConfig'
 
 export async function getNewsletterEntry(id) {
-    console.log("trying to get newsletter entry with id:", id);
+    console.log("trying to get newsletter entry with id:", id);ç
     try {
         const res = await api.get(`/api/newsletter/${id}`);
         console.log("Newsletter entry:", res.data);
